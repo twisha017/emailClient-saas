2a2356a4afdba02e721f1aa48c3b4d17a5f82af1
twisha017 <166981393+twisha017@users.noreply.github.com>
2025-05-21 20:23:06 +0530

    first commit

---
+++ src/app/api/clerk/webhook/route.ts
@@ -0,0 +1,36 @@
+import { db } from "@/server/db";
+
+export const POST = async (req: Request) => {
+    const { data } = await req.json();
+   
+    const emailAddress = data.email_addresses[0].email_address;
+    const firstName = data.first_name;
+    const lastName = data.last_name;
+    const imageUrl = data.image_url;
+    const id = data.id;
+
+    await db.user.create({
+        data:{
+            id:id,
+            emailAddress:emailAddress,
+            firstName:firstName,
+            lastName:lastName,
+            imageUrl:imageUrl,
+            
+        }
+
+    }
+   )
+
+    // await db.user.upsert({
+    //     where: { id },
+    //     update: { emailAddress, firstName, lastName, imageUrl },
+    //     create: { id, emailAddress, firstName, lastName, imageUrl },
+    // });
+
+     console.log(
+        data,'user-created'
+    );
+
+    return new Response('Webhook received', { status: 200 });
+}
