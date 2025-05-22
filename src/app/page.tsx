650c88a51893670a7dd53eddc75613ae56680c49
twisha017 <166981393+twisha017@users.noreply.github.com>
2025-05-22 14:05:30 +0530

    trail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+'use client'
 import { Button } from "@/components/ui/button";
+import {getAurinkoAuthUrl} from '@/lib/aurinko'
 
 export default async function Home() {
 
   return (
-    <Button>hello</Button>
+    <Button onClick={async()=>{
+      const authUrl = await getAurinkoAuthUrl('Google');
+      window.location.href = authUrl
+    }}>Link test</Button>
   );
 }
